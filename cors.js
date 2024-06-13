5c6f5bbecc4d7e52f712cf7befbb3fce43787c08
Ashik70021 <ashik70021@gmail.com>
2024-06-14 01:29:53 +0600

    update cors

--- cors.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res, next) => {
-    try {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Headers", "*");
-        res.setHeader("Access-Control-Allow-Methods", "*");
-    } catch (error) { }
-    next();
-};
